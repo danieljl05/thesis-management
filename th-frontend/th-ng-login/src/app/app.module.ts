868662ca3abbdf13027327f57a4032854c227dc3
Daniel Jiménez <geisondanieljl@gmail.com>
2020-02-23 20:22:53 -0500

    DJ - Login frontend added

---
+++ th-frontend/th-ng-login/src/app/app.module.ts
@@ -0,0 +1,72 @@
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { AppRoutes } from './app.routing';
+import { AppComponent } from './app.component';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FullComponent } from './layouts/full/full.component';
+import { AppHeaderComponent } from './layouts/full/header/header.component';
+import { AppSidebarComponent } from './layouts/full/sidebar/sidebar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DemoMaterialModule } from './demo-material-module';
+
+import { SharedModule } from './shared/shared.module';
+import { SpinnerComponent } from './shared/spinner.component';
+
+// Locale imports
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+import localeEsExtra from '@angular/common/locales/extra/es';
+
+// JWT imports
+import { JwtModule } from "@auth0/angular-jwt";
+import { AuthGuard, LoginGuard, TokenService } from "th-ng-commons";
+
+
+registerLocaleData(localeEs, 'es-CO', localeEsExtra);
+export function tokenGetter() {
+  return localStorage.getItem("th_token");
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FullComponent,
+    AppHeaderComponent,
+    SpinnerComponent,
+    AppSidebarComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    DemoMaterialModule,
+    FormsModule,
+    FlexLayoutModule,
+    HttpClientModule,
+    SharedModule,
+    RouterModule.forRoot(AppRoutes),
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        whitelistedDomains: ["localhost:8000", "project-manager-udi.000webhostapp.com"],
+        blacklistedRoutes: []
+      }
+    }),
+  ],
+  providers: [
+    {
+      provide: LocationStrategy,
+      useClass: PathLocationStrategy
+    },
+    TokenService,
+    AuthGuard,
+    LoginGuard
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
